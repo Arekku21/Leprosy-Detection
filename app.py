54dbc59c3e270131d70fec3a117e2807e150ab5c
Alec Vince <Arekku21@users.noreply.huggingface.co>
2023-09-26 15:15:20 +0000

    added both tabs in 1  and updated function

--- app.py
+++ app.py
@@ -8,13 +8,17 @@ from PIL import Image
 import tempfile
 
 import numpy as np
+
 import math
+
 
 file_urls = [
     'https://www.dropbox.com/scl/fi/onrg1u9tqegh64nsfmxgr/lp2.jpg?rlkey=2vgw5n6abqmyismg16mdd1v3n&dl=1',
-    'https://www.dropbox.com/scl/fi/lvqft5kibtl05e5dank0u/nlp1.jpg?rlkey=gj90a7nsbp0torl81t4ot8h0n&dl=1',
-    'https://www.dropbox.com/scl/fi/adcykczwpvg5ipxl4dhqi/nlp2.jpg?rlkey=0npa4ttmfgld8anqr820yr5js&dl=1',
-    'https://www.dropbox.com/scl/fi/xq103ic7ovuuei3l9e8jf/lp1.jpg?rlkey=g7d9khyyc6wplv0ljd4mcha60&dl=1'
+    'https://www.dropbox.com/scl/fi/xq103ic7ovuuei3l9e8jf/lp1.jpg?rlkey=g7d9khyyc6wplv0ljd4mcha60&dl=1',
+    'https://www.dropbox.com/scl/fi/fagkh3gnio2pefdje7fb9/Non_Leprosy_210823_86_jpg.rf.5bb80a7704ecc6c8615574cad5d074c5.jpg?rlkey=ks8afue5gsx5jqvxj3u9mbjmg&dl=1',
+    'https://www.dropbox.com/scl/fi/gh4zotrzic5y00ok3crje/Non_Leprosy_210823_46_jpg.rf.76c20cb340114a98618ade07c3e6b413.jpg?rlkey=pxdjlhxipmsd12gr4veyg691v&dl=1',
+    'https://www.dropbox.com/scl/fi/r8vgo1xrledlsw7rxq4ar/Tropmed-91-216-g001.jpg?rlkey=6iajn3xoa6zsxtxh4exq4z3p5&dl=1',
+    'https://www.dropbox.com/scl/fi/kxv0q49e92h3fr7ihvqbu/Non_Leprosy_210823_8_jpg.rf.e2d44b96e1bb9b5111b780adec5ba94a.jpg?rlkey=g25iq6vbwqs1glusyv1lgv5a2&dl=1'
 ]
 
 def download_file(url, save_name):
@@ -40,7 +44,7 @@ model = torch.hub.load('ultralytics/yolov5', 'custom', path=path, force_reload=T
 img_path  = [['image_0.jpg'], ['image_1.jpg'],['image_2.jpg'],['image_3.jpg']]
 video_path = [['video.mp4']]
 
-def show_preds_image(image_path):
+def show_preds_image_and_labels(image_path):
     image = cv2.imread(image_path)
 
     results = model(image_path)
@@ -51,12 +55,16 @@ def show_preds_image(image_path):
 
     array_results = array_results.tolist()
 
+    #print raw results
+    print("Raw results: ", array_results)
+
     array_bounding_box= []
 
     array_model_result = []
 
     array_model_confidence = []
 
+    #for labelling bounding box
     for item in array_results:
         array_bounding_box.append([item[0],item[1],item[2],item[3]])
         array_model_result.append(item[6])
@@ -68,50 +76,33 @@ def show_preds_image(image_path):
 
       if array_model_result[numbers] == "Lep":
         # draw a rectangle over the image using the bounding box coordinates
-        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0,255), 1)
-        cv2.putText(image, array_model_result[numbers] + " " +array_model_confidence[numbers] + "%", (x1, y1-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
+
+        #if the value of leprosy conf is < 0.45 then label it as NLp to show the max voting value
+        if  float(array_model_confidence[numbers]) > 45.0:
+
+          cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0,255), 1)
+          cv2.putText(image, array_model_result[numbers] + " " + array_model_confidence[numbers] + "%", (x1, y1-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
+
+        elif float(array_model_confidence[numbers]) < 45.0:
+
+          cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255,0), 1)
+          cv2.putText(image, "Non Lep" + " " + array_model_confidence[numbers] + "%", (x1, y1-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
 
       elif array_model_result[numbers] == "Non Lep":
+        
         # draw a rectangle over the image using the bounding box coordinates
         cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255,0), 1)
-        cv2.putText(image, array_model_result[numbers] + " " +array_model_confidence[numbers] + "%", (x1, y1-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
+        cv2.putText(image, array_model_result[numbers] + " " + array_model_confidence[numbers] + "%", (x1, y1-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
 
-    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    #labelling dictionary
 
-
-inputs_image = [
-    gr.components.Image(type="filepath", label="Input Image"),
-]
-outputs_image = [
-    gr.components.Image(type="numpy", label="Output Image"),
-]
-interface_image = gr.Interface(
-    fn=show_preds_image,
-    inputs=inputs_image,
-    outputs=outputs_image,
-    title="Leprosy Detection",
-    examples=img_path,
-    cache_examples=False,
-)
-
-def labels_image(image_path):
-    image = cv2.imread(image_path)
-
-    results = model(image_path)
-
-    pandas_result = results.pandas().xyxy[0]
-
-    array_results = pandas_result.to_numpy()
-
-    array_results = array_results.tolist()
-    
     array_results_conf_large = []
 
     for yolo_results in array_results:
       if yolo_results[4] > 0.45:
         array_results_conf_large.append(yolo_results)
 
-    print(array_results_conf_large)
+    print("Large results only: ",array_results_conf_large)
 
     num_lep = 0
     confidence_lep = 0
@@ -127,7 +118,6 @@ def labels_image(image_path):
           num_non_lep+=1
           confidence_non_lep += item[4]
 
-
     labels = {}
 
     #if num_lep is more than non lep
@@ -136,14 +126,14 @@ def labels_image(image_path):
     #if num_non_lep is more than lep
     elif num_lep < num_non_lep:
       labels["Non Leprosy"] = round(confidence_non_lep/num_non_lep,2)
-    
+
     #if num_non_lep and num_lep is equal but they are equal coz they are both 0
     elif num_lep == num_non_lep and num_lep == 0:
       labels["Others"] = 0.9
 
     #if num_non_lep and num_lep is equal but they are equal coz they are both 0
     elif num_lep == num_non_lep:
-      
+
       #incase of a tie in quantity we compare the mean probability of each
       confidence_lep = round(confidence_lep/num_lep,2)
       confidence_non_lep = round(confidence_lep/num_non_lep,2)
@@ -153,28 +143,30 @@ def labels_image(image_path):
 
       elif confidence_lep < confidence_non_lep:
         labels["Non Leprosy"] = confidence_non_lep
-      
+
       elif confidence_lep == confidence_non_lep:
         labels["Leprosy"] = confidence_lep
         labels["Non Leprosy"] = confidence_non_lep
 
-    return labels
+
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB),labels
 
 inputs_image = [
     gr.components.Image(type="filepath", label="Input Image"),
 ]
-outputs_label = [
-    gr.outputs.Label(type="label", label="Output Labels"),
+outputs_image = [
+    gr.components.Image(type="numpy", label="Output Image"),
+    gr.components.Label(type="json", label="Labels with Confidence"),
 ]
-interface_label = gr.Interface(
-    fn=labels_image,
+interface_image = gr.Interface(
+    fn=show_preds_image_and_labels,
     inputs=inputs_image,
-    outputs=outputs_label,
-    title="Leprosy detection",
+    outputs=outputs_image,
+    title="Leprosy Detection",
     examples=img_path,
     cache_examples=False,
 )
-
+    
 # def show_preds_video(video_path):
 #     cap = cv2.VideoCapture(video_path)
 #     while(cap.isOpened()):
@@ -216,8 +208,7 @@ interface_label = gr.Interface(
 #     tab_names=['Image inference']
 # ).queue().launch()
 
-
 gr.TabbedInterface(
-    [interface_image,interface_label],
-    tab_names=['Image inference','Label Inference']
+    [interface_image],
+    tab_names=['Image inference']
 ).queue().launch()
